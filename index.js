ee58a32c44529f3fbcc9fc118a28b34f50c31b00
danielpqb <danielpqb@gmail.com>
2022-06-09 18:11:19 -0300

    Tentando passar args

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 let plates = document.getElementsByClassName('prato')
+let drinks = document.getElementsByClassName('bebida')
+let deserts = document.getElementsByClassName('sobremesa')
+
+let choicesIds = []
 
 window.onload = () => {
     //Iterate plates
     for (let i = 0; i < plates.length; i++) {
         let plate = plates[i]
-        plate.onclick = select
-        plate.id = i
-        
-        //nplates.push(plate)
-        //plate.style.backgroundColor = 'red'
+        //plate.onclick.arguments = '(1)'
+        plate.onclick.arguments = select
+        plate.id = 'plate' + i
     }
-    //console.log(nplates)
+
+    //Iterate drinks
+    for (let i = 0; i < drinks.length; i++) {
+        let drink = drinks[i]
+        //drink.onclick.arguments = '(2)'
+        drink.onclick.arguments = select
+        drink.id = 'drink' + i
+    }
+
+    //Iterate deserts
+    for (let i = 0; i < deserts.length; i++) {
+        let desert = deserts[i]
+        //desert.onclick.arguments = '(3)'
+        desert.onclick.arguments = select
+        desert.id = 'desert' + i
+    }
+
     console.log(plates)
+    console.log(drinks)
+    console.log(deserts)
 }
 
-function select() {
-    this.style.border = "5px solid #32B72F"
+function select(type) {
+    switch (type) {
+        case 1:
+            console.log(1)
+            break;
+        case 2:
+            console.log(2)
+            break;
+        case 3:
+            console.log(3)
+            break;
+        default:
+            break;
+    }
+    //if (this.id != false) {
+        //this.style.border = "5px solid #32B72F"
+    //}
+
 }
