734726b202915bbbdf74148a26e723fdaa6fbc43
danielpqb <danielpqb@gmail.com>
2022-06-09 22:29:26 -0300

    Bordas funcionando perfeitamente

--- index.js
+++ index.js
@@ -1,55 +1,32 @@
-let plates = document.getElementsByClassName('prato')
-let drinks = document.getElementsByClassName('bebida')
-let deserts = document.getElementsByClassName('sobremesa')
+let choices = document.getElementsByClassName('escolha')
 
-let choicesIds = []
-
-window.onload = () => {
-    //Iterate plates
-    for (let i = 0; i < plates.length; i++) {
-        let plate = plates[i]
-        //plate.onclick.arguments = '(1)'
-        plate.onclick.arguments = select
-        plate.id = 'plate' + i
+window.onload = () => { //Iterate choices
+    for (let i = 0; i < choices.length; i++) {
+        choices[i].onclick = select
     }
-
-    //Iterate drinks
-    for (let i = 0; i < drinks.length; i++) {
-        let drink = drinks[i]
-        //drink.onclick.arguments = '(2)'
-        drink.onclick.arguments = select
-        drink.id = 'drink' + i
-    }
-
-    //Iterate deserts
-    for (let i = 0; i < deserts.length; i++) {
-        let desert = deserts[i]
-        //desert.onclick.arguments = '(3)'
-        desert.onclick.arguments = select
-        desert.id = 'desert' + i
-    }
-
-    console.log(plates)
-    console.log(drinks)
-    console.log(deserts)
 }
 
-function select(type) {
-    switch (type) {
-        case 1:
-            console.log(1)
-            break;
-        case 2:
-            console.log(2)
-            break;
-        case 3:
-            console.log(3)
-            break;
-        default:
-            break;
+function select() {
+    for (let i = 0; i < this.classList.length; i++) {
+        switch (this.classList[i]) {
+            case 'prato':
+                if (document.querySelector('.select1') != null) {
+                    document.querySelector('.select1').classList.remove('select1')
+                }
+                this.classList.toggle('select1')
+                break;
+            case 'bebida':
+                if (document.querySelector('.select2') != null) {
+                    document.querySelector('.select2').classList.remove('select2')
+                }
+                this.classList.toggle('select2')
+                break;
+            case 'sobremesa':
+                if (document.querySelector('.select3') != null) {
+                    document.querySelector('.select3').classList.remove('select3')
+                }
+                this.classList.toggle('select3')
+                break;
+        }
     }
-    //if (this.id != false) {
-        //this.style.border = "5px solid #32B72F"
-    //}
-
 }
