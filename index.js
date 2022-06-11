e97e03272820f170cabd6a8788095c7e643bc268
Daniel <danielpqb@gmail.com>
2022-06-10 21:36:11 -0300

    Quase perfeito

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let choices = document.getElementsByClassName('escolha')
-
-window.onload = () => { //Iterate choices
-    for (let i = 0; i < choices.length; i++) {
-        choices[i].onclick = select
+class Order {
+    constructor(plateInfo, drinkInfo, desertInfo, finalPrice) {
+        this.plateInfo = plateInfo
+        this.drinkInfo = drinkInfo
+        this.desertInfo = desertInfo
+        this.finalPrice = finalPrice
     }
 }
 
@@ -10,18 +11,21 @@ function select() {
     for (let i = 0; i < this.classList.length; i++) {
         switch (this.classList[i]) {
             case 'prato':
+                order.plateInfo = [parseFloat(this.querySelector('.preco').innerHTML.replace(/[^\d,]/g, '').replace(',', '.')), this.querySelector('h1').innerHTML]
                 if (document.querySelector('.select1') != null) {
                     document.querySelector('.select1').classList.remove('select1')
                 }
                 this.classList.toggle('select1')
                 break;
             case 'bebida':
+                order.drinkInfo = [parseFloat(this.querySelector('.preco').innerHTML.replace(/[^\d,]/g, '').replace(',', '.')), this.querySelector('h1').innerHTML]
                 if (document.querySelector('.select2') != null) {
                     document.querySelector('.select2').classList.remove('select2')
                 }
                 this.classList.toggle('select2')
                 break;
             case 'sobremesa':
+                order.desertInfo = [parseFloat(this.querySelector('.preco').innerHTML.replace(/[^\d,]/g, '').replace(',', '.')), this.querySelector('h1').innerHTML]
                 if (document.querySelector('.select3') != null) {
                     document.querySelector('.select3').classList.remove('select3')
                 }
@@ -38,17 +42,30 @@ function select() {
         bi.onclick = sendMsg
         bi.querySelector('div').innerHTML = 'Fechar pedido'
     }
-
 }
 
 function sendMsg() {
+    order.finalPrice = order.plateInfo[0] + order.drinkInfo[0] + order.desertInfo[0]
+    
     let msg = 'Olá, gostaria de fazer o pedido:\n' +
-        '- Prato: Frango Yin Yang\n' +
-        '- Bebida: Coquinha Gelada\n' +
-        '- Sobremesa: Pudim\n' +
-        'Total: R$ ' + preco
+        '- Prato: ' + order.plateInfo[1] + '\n' +
+        '- Bebida: ' + order.drinkInfo[1] + '\n' +
+        '- Sobremesa: ' + order.desertInfo[1] + '\n' +
+        'Total: R$ ' + order.finalPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
-    msg = encodeURIComponent(msg)
+    console.log(msg)
 
-    window.location.href = 'https://wa.me/5521994149949?text=' + msg
+    //msg = encodeURIComponent(msg)
+
+    //window.location.href = 'https://wa.me/5521994149949?text=' + msg
 }
+
+let order = new Order()
+let choices = document.getElementsByClassName('escolha')
+
+window.onload = () => { //Iterate choices
+    for (let i = 0; i < choices.length; i++) {
+        choices[i].onclick = select
+    }
+}
+
